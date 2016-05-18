1ea19231908684d381ef1840865242b233213cb5
mort <martid0311@gmail.com>
2016-05-18 19:22:30 +0200

    rewrite

--- server.js
+++ server.js
@@ -1,98 +1,35 @@
-var atob = require("atob");
+var spawn = require("child_process").spawn;
+var urllib = require("url");
 var fs = require("fs");
-var status = require("statuses");
-var formidable = require("formidable");
-var wrench = require("wrench");
-var express = require("express");
-var colors = require("colors");
+var Server = require("./kattie");
 
-var util = require("./js/util");
-var HttpStreamer = require("./js/http-streamer");
-var PortManager = require("./js/port-manager");
-var MediaPlayer = require("./js/media-player");
-var torrentPlayer = require("./js/torrent-player");
+var conf = JSON.parse(fs.readFileSync("conf.json"));
 
-colors.setTheme({
-	info: "grey",
-	error: "red",
-	ok: "green"
-});
+var server = Server({ port: conf.port, streamPort: conf.stream_port });
 
-var conf = JSON.parse(fs.readFileSync("conf.json", "utf8"));
+server.onstream = function(name, req, res) {
+	var from = urllib.parse(req.headers.referer);
 
-//Create tmp/ if necessary, and clear it if it's not already empty
-try {
-	fs.mkdirSync("tmp");
-} catch (err) {
-	if (err && err.code !== "EEXIST")
-		throw err;
-}
-fs.readdirSync("tmp").forEach(function(file) {
-	fs.unlinkSync("tmp/"+file);
-});
+	var url = "http://localhost:"+conf.stream_port;
+	spawn("notify-send", ["Playing "+name]);
 
-var portManager = new PortManager(conf.port);
+	var child = spawn(conf.player_command, [
+		"--fullscreen",
+		"--play-and-exit",
+		"-I", "http",
+		"--http-port", conf.player_port,
+		"--http-password", "media",
+		"--",
+		url
+	]);
 
-//Initialize web app
-var app = express();
-app.use(express.static("web"));
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
 
-//Helper for form data parsing
-function parseFormData(req, res, cb) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-		if (err)
-			return res.json({error: err.toString()});
-
-		var rFiles = {};
-		for (var i in files) {
-			rFiles[i] = new parseFormData.File(files[i]);
-		}
-
-		if (!rFiles.subs)
-			rFiles.subs = {};
-
-		cb(fields, rFiles);
-	});
-}
-parseFormData.File = function(options) {
-	this.path = options.path;
-};
-parseFormData.File.prototype.read = function(cb, charset) {
-	fs.readFile(this.path, charset, cb);
-};
-
-//Magnet link endpoint
-app.post("/view/magnet/:href", function(req, res) {
-	parseFormData(req, res, function(fields, files) {
-		var source = decodeURIComponent(req.params.href);
-		torrentPlayer.play(source, res, files.subs.path, portManager, conf);
-	});
-});
-
-//URL endpoint
-app.post("/view/url/:href", function(req, res) {
-	parseFormData(req, res, function(fields, files) {
-		var url = decodeURIComponent(req.params.href);
-		var player = new MediaPlayer(url, files.subs.path, portManager.getPort(), conf);
-		setTimeout(function() {
-			res.json({redirect: "http://"+conf.host+":"+player.port.val});
-		}, 2000);
-	});
-});
-
-//Torrent file endpoint
-app.post("/view/torrent", function(req, res) {
-	parseFormData(req, res, function(fields, files) {
-		files.file.read(function(err, body) {
-			if (err)
-				return res.json({error: err.toString()});
-
-			torrentPlayer.play(body, res, files.subs.path, portManager, conf);
+	setTimeout(() => {
+		res.writeHead(302, {
+			location: "http://"+from.hostname+":8082"
 		});
-	});
-});
-
-var port = portManager.getPort();
-app.listen(port.val);
-console.log(("Listening to port "+port.val+".").ok);
+		res.end();
+	}, 1000);
+}
